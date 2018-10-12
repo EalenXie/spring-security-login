ab2bfe1477834412516a04ee5ce67ef5d21f0343
EalenXie <ealenxie@qq.com>
2018-10-12 12:42:33 +0800

    补充注释

--- src/main/java/com/wuxicloud/security/SecurityUser.java
+++ src/main/java/com/wuxicloud/security/SecurityUser.java
@@ -36,21 +36,25 @@ public class SecurityUser extends User implements UserDetails {
         return authorities;
     }
 
+    //账户是否未过期,过期无法验证
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    //指定用户是否解锁,锁定的用户无法进行身份验证
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    //指示是否已过期的用户的凭据(密码),过期的凭据防止认证
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    //是否可用 ,禁用的用户不能身份验证
     @Override
     public boolean isEnabled() {
         return true;
