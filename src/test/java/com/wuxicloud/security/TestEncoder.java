41711d8c4abde97cb0c2906927bb93a20816db70
EalenXie <ealenxie@qq.com>
2019-12-11 13:20:30 +0800

    删除demo的线程池配置

--- src/test/java/com/wuxicloud/security/TestEncoder.java
+++ src/test/java/com/wuxicloud/security/TestEncoder.java
@@ -3,8 +3,6 @@ package com.wuxicloud.security;
 import org.junit.Test;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.UUID;
-
 /**
  * Created by EalenXie on 2018/7/11 15:13
  */
